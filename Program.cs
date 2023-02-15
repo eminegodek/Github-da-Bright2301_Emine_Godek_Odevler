17e9369177630253aac665f3d431bf828f6942f1
eminegodek <124085009+eminegodek@users.noreply.github.com>
2023-02-15 13:38:54 +0300

    Add files via upload
    2 file

--- Program.cs
+++ Program.cs
@@ -1,30 +1,15 @@
-﻿namespace Odev01_04_02_2023
+﻿namespace Odev03_04_02_2023
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Birinci sayiyi giriniz.: ");
-            int sayi1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("İkinci sayiyi giriniz: ");
-            int sayi2 = Convert.ToInt32(Console.ReadLine());
-            int sonuc = sayi1 / sayi2;
-            Console.WriteLine(sonuc);
-            if (sonuc >= 50)
-            {
-                Console.WriteLine("Geçti");
-
-            }
-            else
-            {
-                Console.WriteLine("Kaldı");
-            }
-
+            int sayi = 0;
+            Console.Write("Sayı giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Karesi: {0}", Math.Pow(sayi, 2));
+            Console.WriteLine("Küpü: {0}", Math.Pow(sayi, 3));
+            Console.ReadKey();
         }
-
-
-        
-         
-
     }
 }
