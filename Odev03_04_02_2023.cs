0f6ca5cc587796b7c696e2d22c9264844aad2458
eminegodek <124085009+eminegodek@users.noreply.github.com>
2023-02-15 14:02:04 +0300

    Add files via upload

---
+++ Odev03_04_02_2023.cs
@@ -0,0 +1,15 @@
+﻿namespace Odev03_04_02_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            Console.Write("Sayı giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Karesi: {0}", Math.Pow(sayi, 2));
+            Console.WriteLine("Küpü: {0}", Math.Pow(sayi, 3));
+            Console.ReadKey();
+        }
+    }
+}
