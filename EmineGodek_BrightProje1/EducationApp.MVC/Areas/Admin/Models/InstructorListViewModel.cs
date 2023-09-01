c1442b1554befdcdf54440194e5700916339d680
eminegodek <eminegodekk@gmail.com>
2023-09-02 00:28:48 +0300

    revize

--- EmineGodek_BrightProje1/EducationApp.MVC/Areas/Admin/Models/InstructorListViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EducationApp.MVC.Areas.Admin.Models
-{
-    public class InstructorListViewModel
-    {
-        public List<InstructorViewModel> InstructorViewModelList { get; set; }
-        public string SourceAction { get; set; }
-    }
-}
--- EmineGodek_BrightProje1/EducationApp.MVC/Areas/Admin/Models/ProductListViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EducationApp.MVC.Areas.Admin.Models
-{
-    public class ProductListViewModel
-    {
-        public List<ProductViewModel> ProductViewModelList { get; set; }
-        public string SourceAction { get; set; }
-    }
-}
